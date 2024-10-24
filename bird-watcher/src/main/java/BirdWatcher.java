ef8aa9d8afda4e2f494db0e3701b1008ea7d1761
Rodrigo Donizete Santana de Pádua <rodrigo.dsp@gmail.com>
2024-10-24 18:42:48 -0300

    Bird Watcher exercise

---
+++ bird-watcher/src/main/java/BirdWatcher.java
@@ -0,0 +1,47 @@
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return birdsPerDay;
+    }
+
+    public int getToday() {
+        return birdsPerDay[birdsPerDay.length - 1];
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length - 1]++;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        for (int birds : birdsPerDay) {
+            if (birds == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        int count = 0;
+        for (int i = 0; i < numberOfDays && i < birdsPerDay.length; i++) {
+            count += birdsPerDay[i];
+        }
+        return count;
+    }
+
+    public int getBusyDays() {
+        int count = 0;
+        for (int birds : birdsPerDay) {
+            if (birds >= 5) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
